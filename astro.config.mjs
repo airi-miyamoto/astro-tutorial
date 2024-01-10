aabf25f4e8ddf81f74b8fdf28226085d6b0319b3
a.miyamoto <a.miyamoto@grow-group.jp>
2024-01-10 22:41:50 +0900

    tutorial complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://marvelous-jalebi-35cc8c.netlify.app/"
+  site: "https://marvelous-jalebi-35cc8c.netlify.app/",
+  integrations: [preact()]
 });
